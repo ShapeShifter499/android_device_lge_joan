dd2c3be0074e5d08fb532ee5d01b3b3f7576c7b4
shapeshifter499 <gero3977@gmail.com>
2020-10-07 08:48:43 -0700

    Revert "h930: add updatable_apex.mk"
    This reverts commit 9ef76ca60b6809e00bd70cc4c9a56a7038a9a95d.
    
    Convert lineage-17.1 branch to lineage-16.0 for Pie blobs

--- lineage_joan.mk
+++ lineage_joan.mk
@@ -15,9 +15,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Device identifiers
 PRODUCT_DEVICE := joan
 PRODUCT_NAME := lineage_joan
