0d881073a35b13c53e5fb7f33a298081ab0caee0
SupremMotu <francisd132435@gmail.com>
2020-10-08 00:22:43 -0700

    joan: pass safetynet
    Change-Id: Icc646693f3f3ec08a239ccc28d5145c4e2c1f499

--- lineage_joan.mk
+++ lineage_joan.mk
@@ -23,9 +23,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="joan" \
     PRODUCT_DEVICE="joan" \
     PRODUCT_NAME="joan_global_com" \
-    PRIVATE_BUILD_DESC="joan_global_com-user 8.0.0 OPR1.170623.026 190211754b47c release-keys"
+    PRIVATE_BUILD_DESC="joan_global_com-user 8 OPR1.170623.026 181381736b4e9 release-keys"
 
-BUILD_FINGERPRINT="lge/joan_global_com/joan:8.0.0/OPR1.170623.026/190211754b47c:user/release-keys"
+BUILD_FINGERPRINT="lge/joan_global_com/joan:8.0.0/OPR1.170623.026/181381736b4e9:user/release-keys" 
 
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += ro.product.model ro.vendor.product.model
 
